bcd9bd302b573f0c939805d0e765378994927556
arvinmi <64492021+arvinmi@users.noreply.github.com>
2025-04-08 17:53:26 -0700

    some config changes

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -1,5 +1,12 @@
 -- config from kickstart.nvim, jonhoo, mitchellh, sumitdotml
 
+if vim.g.vscode then
+  -- VSCode Neovim
+  require "user.vscode_keymaps"
+else
+  -- Ordinary Neovim
+end
+
 -- set leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
